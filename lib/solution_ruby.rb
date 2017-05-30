bfecdc3d08b27fd47e6afaf87e7fefee9bf8088f
matthiashaefeli <haefeli.matthias@gmail.com>
2017-05-30 09:03:37 -0500

    rspec and solution to problem 4

--- lib/solution_ruby.rb
+++ lib/solution_ruby.rb
@@ -8,9 +8,26 @@ class Solution
 	  return array.join('')
 	end
 
+	def factorial(n)
+	  result = 1
+	  while n > 0
+	    result = result * n
+	    n -= 1
+	  end
+	  return result
+	end
 
+	def longest_word(sentence)
+	  result = sentence.split(' ').sort_by {|word| word.length}
+	  result[-1]
+	end
 
-
-
-	
+	def sum_nums(num)
+	  result = 0 
+	  until num == 0 
+	    result += num 
+	    num-=1
+	  end
+	  return result
+	end
 end
--- spec/solution_ruby_spec.rb
+++ spec/solution_ruby_spec.rb
@@ -4,12 +4,54 @@ describe Solution do
 	solution = Solution.new
 
 	describe "reverse" do
+
 		it "gives a string reverse" do 
 			expect(solution.reverse("hello")).to eq "olleh"
 		end
 
 		it "gives a string reverse" do 
 			expect(solution.reverse("this is great")).to eq "taerg si siht"
+		end
+	end
+
+	describe "factorial" do 
+
+		it "gives an integer 'n' and should return n*(n-1)*(n-2)*...*2*1" do 
+			expect(solution.factorial(2)).to eq 2
+		end
+
+		it "gives an integer 'n' and should return n*(n-1)*(n-2)*...*2*1" do 
+			expect(solution.factorial(8)).to eq 40320
+		end
+
+		it "As a special case, factorial(0) == 1" do 
+			expect(solution.factorial(0)).to eq 1
+		end
+	end
+
+	describe "longest word" do 
+
+		it "takes a string and returns the longest word in the string" do 
+			expect(solution.longest_word("hello all")).to eq "hello"
+		end 
+
+		it "takes a string and returns the longest word in the string" do 
+			expect(solution.longest_word("this is the best solution ever")).to eq "solution"
+		end 
+
+		it "takes a string and returns the longest word in the string" do 
+			expect(solution.longest_word("one")).to eq "one"
+		end 
+	end
+
+	describe "sum nums" do 
+
+		it "takes in an integer `num` and returns the sum of all integers between zero and num" do 
+			expect(solution.sum_nums(5)).to eq 15
+		end
+
+		it "takes in an integer `num` and returns the sum of all integers between zero and num" do 
+			expect(solution.sum_nums(20)).to eq 210
 		end
 	end
 
