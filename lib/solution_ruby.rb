bd0730aeddd6ecfc59952b5bb92465341e5a42b3
matthiashaefeli <haefeli.matthias@gmail.com>
2017-05-30 15:26:37 -0500

    finsihed ruby solution

--- lib/solution_ruby.rb
+++ lib/solution_ruby.rb
@@ -243,5 +243,8 @@ class Solution
 	end
 
 	def num_repeats(string)
+		string_array = string.split("")
+		new_array = string_array.select{ |letter| string_array.count(letter) > 1}
+		return new_array.uniq.count
 	end
 end
